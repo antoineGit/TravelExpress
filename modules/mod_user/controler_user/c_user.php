35113b54957580d13446f72b95bfeae0efe0b0e4
antoineGit <auer.antoine@gmail.com>
2015-03-23 17:58:07 -0400

    Affichage des infos de l'utilisateur en cours

---
+++ modules/mod_user/controler_user/c_user.php
@@ -0,0 +1,16 @@
+<?php
+$module = "user";
+require_once ("./modules/mod_$module/view_$module/v_$module.php");
+require_once ("./modules/mod_$module/model_$module/m_$module.php");
+class ControlerUser extends ControlerGeneric {
+    function printUserInfo(){
+        
+
+    }
+
+
+}
+
+
+
+?>
---
+++ modules/mod_user/mod_user.php
@@ -0,0 +1,27 @@
+<?php
+class Moduleuser extends ModuleGeneric{
+  public function __construct(){
+    $module = "user";
+    $action = "voir";
+    if(isset($_GET['action']))
+      $action = $_GET['action'];
+
+    switch($action){
+        case 'voir':
+            require_once ("modules/mod_$module/controler_$module/c_user.php");
+            $this->controler = new ControlerUser();
+            $this->controler->printUserInfo();
+        break;
+
+    }
+  }
+
+
+
+}
+
+
+
+
+
+?>
